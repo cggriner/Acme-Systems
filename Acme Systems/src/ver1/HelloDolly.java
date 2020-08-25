4f580db664279d06be7b9815db8069ed13b01fbb
Cole Griner <colegriner35@gmail.com>
2020-08-25 18:31:40 -0400

    Added a println statement.

--- Acme Systems/src/ver1/HelloDolly.java
+++ Acme Systems/src/ver1/HelloDolly.java
@@ -4,7 +4,7 @@ public class HelloDolly {
 
 	public static void main(String[] args) {
 		System.out.println("This is a print statement.");
-
+		System.out.println("Hello Dolly from Eclipse again");
 	}
 
 }
